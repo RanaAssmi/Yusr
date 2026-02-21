3b098774ab48e6ea5f6612f26c239901f6b203d2
RanaAssmi <444809906@kku.edu.sa>
2026-02-21 23:44:09 +0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -5,5 +5,5 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_ANON_KEY,
   { auth: { persistSession: true, autoRefreshToken: true } }
 );
-VITE_SUPABASE_URL: https://ekmnyixxbqpdkzyaclsg.supabase.co
-VITE_SUPABASE_ANON_KEY: sb_publishable_BiUfauDvCCkrcvYKMmyZzQ_0T9SUVCV
+VITE_SUPABASE_URL=https://gcmnaugklfvueaqgwawt.supabase.co
+VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdjbW5hdWdrbGZ2dWVhcWd3YXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0OTQwODgsImV4cCI6MjA3NTA3MDA4OH0.Kl2CY5GYOnPIZLd0ZA9r7dyPNDvRgzxGZugRq1IkwJA
