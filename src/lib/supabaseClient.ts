5d6e3b8fa69bea16559fd81316b47649a5c41943
RanaAssmi <444809906@kku.edu.sa>
2026-02-22 07:34:24 +0300

    Update supabaseClient.ts

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -5,5 +5,5 @@ export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_ANON_KEY,
   { auth: { persistSession: true, autoRefreshToken: true } }
 );
-VITE_SUPABASE_URL=https://ekmnyixxbqpdkzyaclsg.supabase.co
-VITE_SUPABASE_ANON_KEY=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrbW55aXh4YnFwZGt6eWFjbHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2ODgzOTQsImV4cCI6MjA4NzI2NDM5NH0.GGA38ja1mht_BtRVNbeBEFXSddzxNXcJMZIjSaK3kP0
+VITE_SUPABASE_URL= https;//ekmnyixxbqpdkzyaclsg.supabase.co
+VITE_SUPABASE_ANON_KEY =eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrbW55aXh4YnFwZGt6eWFjbHNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE2ODgzOTQsImV4cCI6MjA4NzI2NDM5NH0.GGA38ja1mht_BtRVNbeBEFXSddzxNXcJMZIjSaK3kP0
